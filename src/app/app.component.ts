7713664c886e167f4aa9c1f3e507c2c757aa851a
Darshana Prahath <90643832+darshana-prabath@users.noreply.github.com>
2023-06-25 01:38:18 +0530

    My card is completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,96 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CardsService } from './service/cards.service';
+import { Card } from './models/card.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'cards';
+  cards:Card[]=[];
+  card: Card={
+    id:'',
+    cardNumber:'',
+    cardholderName:'',
+    expiryMonth:'',
+    expiryYear:'',
+    cvc:'',
+
+  }
+
+constructor(private cardsService: CardsService){
+
+}
+  ngOnInit(): void {
+    this.getAllCards();
+    
+
+  }
+
+  getAllCards(){
+    this.cardsService.getAllCards()
+    .subscribe(
+      response =>{
+        this.cards=response;
+        this.card = {
+          id:'',
+          cardNumber:'',
+          cardholderName:'',
+          expiryMonth:'',
+          expiryYear:'',
+          cvc:'',
+      
+        }
+      }
+    )
+  }
+  onSubmit (){
+
+     if(this.card.id === ''){
+      this.cardsService.addCard(this.card)
+      .subscribe(
+        response =>{
+          this.getAllCards();
+          this.card = {
+            id:'',
+            cardNumber:'',
+            cardholderName:'',
+            expiryMonth:'',
+            expiryYear:'',
+            cvc:'',
+        
+          };
+          
+        }
+      );
+     }
+     else{
+      this.updateCard(this.card);
+     }
+  }
+
+
+  deleteCard(id: string){
+    this.cardsService.deleteCard(id)
+    .subscribe(
+      response =>{
+        this.getAllCards();
+      }
+    )
+  }
+
+  populateForm(card: Card) {
+    this.card = card;
+  }
+  updateCard(card: Card){
+    this.cardsService.updateCard(card)
+    .subscribe(
+      response =>{
+        this.getAllCards();
+      }
+    )
+  }
+
 }
